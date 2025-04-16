ebca2097b39cf0de828d58736618648326581e5e
anish1A1 <alloozpat87@gmail.com>
2025-04-16 11:54:43 +0545

    added nextjs as the frontend in the project

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e153777257353296857d6ff2d36891d9e5dcbf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b948d5e3d4c68a907777d0a8899f1f4e091c2e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
