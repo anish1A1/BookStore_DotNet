870133261e49eb0ef38b05939cd9e14b8d7d51d9
anish1A1 <alloozpat87@gmail.com>
2025-04-21 08:58:48 +0545

    added new folders and added codes to connect to database and also integrated JWT

---
+++ backend/Data/ApplicatioDbContext.cs
@@ -0,0 +1,15 @@
+using System;
+using backend.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data;
+
+public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+}
+
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,20 +1,63 @@
+using System.Text;
+using backend.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<ApplicationDbContext>(o =>
+    o.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")))
+    ;
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+builder.Services.AddAuthentication(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.ASCII.GetBytes(builder.Configuration["JwtSettings:Secret"] ?? "YourSuperSecretKeyHereMakeItLongAndSecure12345")
+        ),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
+
+
+// Configure authorization policies
+builder.Services.AddAuthorization(options =>
+{
+    // Policy for Admin role
+    options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+
+    // Policy for User role
+    options.AddPolicy("RequireUserRole", policy => policy.RequireRole("User"));
+});
+
+
+
+// Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
 
 app.UseHttpsRedirection();
 
